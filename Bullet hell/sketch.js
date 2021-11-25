d0eee334ae548b4424a785d677e0b294b327b442
Fakeintelligence <85872036+Fakeintelligence@users.noreply.github.com>
2021-11-25 17:21:12 +0800

    Add files via upload

--- Bullet hell/sketch.js
+++ Bullet hell/sketch.js
@@ -1,8 +1,10 @@
 var backgroundTop, backgroundBottom, backgroundLeft, backgroundRight, backgroundCenter;
 var pm, pb, pl, pr, pt;
 var player;
-var mob;
+var mobImg;
 var safebox;
+var X, Y;
+var mobY, mobX
 function preload() {
 backgroundBottom = loadImage("background_bottom.png");
 backgroundTop = loadImage("background_top.png");
@@ -10,6 +12,7 @@ backgroundLeft = loadImage("background_left.png");
 backgroundRight = loadImage("background_right.png");
 backgroundCenter = loadImage("background_central.png");
 playerImg = loadImage("player.png");
+mobImg = loadImage("mob.png");
 };
 
 function setup() {
@@ -34,32 +37,69 @@ pt = createSprite(100, 30, 70, 70);
 pt.addImage("bt",backgroundTop);
 pt.scale = 0.27;
 
+X = 80;
+Y = 190;
+
+
+
 safebox = createSprite(80, 190, 25, 25);
 //safebox.fill();
-
-player = createSprite(80, 190, 10, 10);
-player.addImage("p",playerImg);
-player.scale = 0.01;
-
-
-
 
 };
 
 function draw() {
   background('gray');
 
-  if(keyDown("space")) {
-    player.y +=10;
-    console.log("space key pressed")
-}
+player = createSprite(X, Y, 10, 10);
+player.addImage("p",playerImg);
+player.scale = 0.01;
 
-console.log(player.x, player.Y)
+  if(keyDown("d")) {
+   X +=1
+};
+  if(keyDown("w")) {
+  Y -=1
+};
+  if(keyDown("a")) {
+  X -=1
+};
+  if(keyDown("s")) {
+  Y +=1
+};
 
+console.log(X, Y);
+
+  mobSpawn();
+  mobChase();
   drawSprites();
 };
 
+function mobChase(){
+  if(Y>mobY){
+    mobY +=1
+  };
 
+  if(Y<mobY){
+    mobY -=1
+  };
+
+  if(X>mobX){
+    mobX +=1
+  };
+
+  if(X<mobX){
+    mobX -=1
+  };
+};
+
+function mobSpawn(){
+  var mobs = createSprite(mobX, mobY, 10, 10);
+  mobY = Math.round(random(100, 200));
+  mobX = Math.round(random(100, 200));
+  console.log(mobX, mobY)
+  mobs.addImage("m", mobImg);
+  mobs.scale = 0.01;
+}
 
 
 
